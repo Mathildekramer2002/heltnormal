37f6d0e4ac1ee956e1a4c5f7955edd7d57440fa5
Marielouise22 <marielouise@dahlmadsen.dk>
2026-05-26 12:28:46 +0200

    Array i myterogfakta
    Array lavet i myter og fakta

---
+++ js/myterogfakta.js
@@ -0,0 +1,20 @@
+"use strict"; 
+
+// Her kommer vores array 
+const udsagnListe = [
+
+    {
+        id: 1,
+        udsagn: "Mennesker med psykiske lidelser kan ikke få et normalt liv.",
+        svar: "Myte",
+        forklaring: "Mange lever gode og stabile liv med uddannelse, job, familie og venner især med den rette støtte og behandling."
+    },
+
+    {
+        id: 2,
+        navn: "Personer med ADHD kan ikke koncentrere sig.",
+        side: "Myte",
+        forklaring: "Personer med ADHD kan ofte koncentrere sig godt om aktiviteter, der interesserer dem meget. Udfordringen handler typisk om at regulere og styre opmærksomheden, ikke om at mangle evnen til at koncentrere sig."
+    },
+
+];
