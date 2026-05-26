dd2a86bd2902855f01d5a02cbee211db5e7c3d60
Mathildekramer2002 <Mathilde.mj@hotmail.com>
2026-05-26 09:29:07 +0200

    Array
    Array oprettet til vores "bobler" på forsiden

--- js/Forside.js
+++ js/Forside.js
@@ -1,3 +1,22 @@
 "use strict";
 
+// Her kommer vores array 
+const bobler = [
+
+    {
+        id: 1,
+        navn: "Myter og fakta"
+    },
+
+    {
+        id: 2,
+        navn: "Personlige historier"
+    },
+
+    {
+        id: 3,
+        navn: "Statistikker"
+    }
+
+];
 
