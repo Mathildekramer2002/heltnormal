f1027ae8ff79e4e7d5643fd578161971b20f4a70
Mathildekramer2002 <Mathilde.mj@hotmail.com>
2026-05-26 11:06:36 +0200

    Dynamiske bobler
    der lavet "bobler" ud fra vores array med forEach. Hver bobbel bliver oprettet automatisk i JavaScript og tilføjet til HTML med appendChild. Boblerne får deres navn fra arrayet og har fået en klikfunktion, så brugeren bliver sendt videre til forskellige sider.

--- js/Forside.js
+++ js/Forside.js
@@ -25,3 +25,30 @@ const bobler = [
 
 ];
 
+
+// Her kalder vi på vores bobbel container i vores html
+const container = document.querySelector ("#bobbelContainer");
+
+// Her laver vi et foreach loop der gennemgår hver bobbel
+bobler.forEach(boblerne => {
+
+    // Her opretter vi en ny div for hver bobbel
+    const bobbel = document.createElement("div");
+
+    // Her giver vi boblerne en klasse, så vi kan style dem på én gang i CSS
+    bobbel.classList.add("bobbel");
+
+    // her tilføjer vi et navn til hver bobbel
+    bobbel.textContent = boblerne.navn;
+
+    // Her sætter vi en lytter på på boblerne, så de reagere på klik
+    bobbel.addEventListener("click", () => {
+
+        // Her sendes brugeren videre ved klik på boblen
+        window.location.href = boblerne.side;
+    });
+
+    // Her lægger vi vores bobler ind i vores container i vores html
+    container.appendChild(bobbel);
+
+});
