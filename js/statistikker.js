8bfcfd2682ef922956c936486727b10fe75ad61a
fmlk1506 <eaa25fmlk@students.eaaa.dk>
2026-05-27 09:43:40 +0200

    JavaScript forbundet med HTML
    Her har vi forbundet vores JS med vores HTML elementer på vores statistik side

--- js/statistikker.js
+++ js/statistikker.js
@@ -4,8 +4,7 @@
 const statistikker = [
   {
     id: 1,
-    udsagn:
-      "Hvor mange børn og unge får en psykisk lidelse inden de fylder 18 år?",
+    udsagn: "Hvor mange børn og unge får en psykisk lidelse inden de fylder 18 år?",
     svar: 15,
     total: 100,
     forklaring:
@@ -26,3 +25,27 @@ const statistikker = [
     forklaring: "En undersøgelse viser at 87% af de adspurgte, har skjult deres psykiske lidelse grundet tidligere negative erfaringer med at være åben.",
   },
 ];
+
+// Her kalder vi på alle elementerne, som vi skal bruge fra vores HTML
+const introBoks = document.querySelector("#introBoks");
+const spilBoks = document.querySelector("#spilBoks");
+const startBtn = document.querySelector("#startBtn");
+const udsagn = document.querySelector("#udsagn");
+const personer = document.querySelector("#personer");
+const videreBtn = document.querySelector("#videreBtn");
+const forklaringBoks = document.querySelector("#forklaringBoks");
+const forklaring = document.querySelector("#forklaring");
+
+// Her laver vi en variabel, der holder styr på, hvilken statistik brugeren er nået til 
+let nuvaerende = 0; 
+
+// Her laver vi en variabel der gemmer brugerens svar 
+let brugerSvar = 0;
+
+// Vi sætter en lytter på startknappen, som kalder på funktionen "start spil", når der klikkes
+startBtn.addEventListener("click", startSpil);
+
+// Vi sætter en lytter på videreknappen, som kalder på funktionen "næste statistik", når der klikkes
+videreBtn.addEventListener("click", naesteStatistik);
+
+
